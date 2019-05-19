94cbd6afb9967f8afddfa0933c2e9bce94e21bd7
Gauthier Delamarre <gdelamarre@gmail.com>
2019-05-19 11:57:17 +0200

    Add some basic features and tests

--- src/MultilingualString.php
+++ src/MultilingualString.php
@@ -4,15 +4,32 @@
 namespace Gauthier\MultilingualString;
 
 
+/**
+ * Class MultilingualString
+ * @package Gauthier\MultilingualString
+ */
 class MultilingualString
 {
+    /**
+     * @var array
+     */
     protected static $availableLanguages = [];
 
-    protected static $currentLanguage;
-
+    /**
+     * @var
+     */
     protected static $defaultLanguage;
 
+    /**
+     * @var array
+     */
     protected static $routes = [];
+
+    /**
+     * @var string
+     */
+    protected static $fallbackLanguage;
+
     /**
      * @var array
      */
@@ -28,13 +45,19 @@ class MultilingualString
     }
 
 
+    /**
+     * @param string $language
+     */
     public static function addAvailableLanguage(string $language)
     {
-        if(!in_array($language, self::$availableLanguages)) {
+        if (!in_array($language, self::$availableLanguages)) {
             self::$availableLanguages[] = $language;
         }
     }
 
+    /**
+     * @param array $languages
+     */
     public static function setAvailableLanguages(array $languages)
     {
         self::$availableLanguages = [];
@@ -42,6 +65,54 @@ class MultilingualString
             self::addAvailableLanguage($language);
         }
     }
+
+    public static function setRoute(string $from, string $to)
+    {
+        self::$routes[$from] = $to;
+    }
+
+    public static function setFallbackLanguage(string $language)
+    {
+        self::validateLanguage($language);
+
+        self::$fallbackLanguage = $language;
+    }
+
+    /**
+     * @return mixed
+     * @throws MultilingualStringException
+     */
+    protected static function getCurrentLanguage()
+    {
+        if(empty(self::$currentLanguage)) {
+            self::$currentLanguage = self::getDefaultLanguage();
+        }
+
+        return self::$currentLanguage;
+    }
+
+    /**
+     * @param $lang
+     * @return mixed|null
+     */
+    public static function getRoutedLanguage($lang)
+    {
+        return self::$routes[$lang] ?? null;
+    }
+
+    /**
+     * @return mixed|string
+     * @throws MultilingualStringException
+     */
+    public static function getFallbackLanguage()
+    {
+        if (empty(self::$fallbackLanguage)) {
+            return self::getDefaultLanguage();
+        }
+
+        return self::$fallbackLanguage;
+    }
+
 
     /**
      * @param string $language
@@ -56,38 +127,93 @@ class MultilingualString
 
     /**
      * @param string $language
-     * @throws MultilingualStringException
-     */
-    public static function setCurrentLanguage(string $language)
-    {
-        self::validateLanguage($language);
-
-        self::$currentLanguage = $language;
-    }
-
-    /**
-     * @param string $language
      * @return bool
      * @throws MultilingualStringException
      */
     public static function validateLanguage(string $language)
     {
-        if(!in_array($language, self::$availableLanguages)) {
-            throw new MultilingualStringException(sprintf('Cannot set "%s" as default language because this is not one of declared available languages (%s)', $language, implode(', ', self::$availableLanguages)));
+        if (!in_array($language, self::$availableLanguages)) {
+            throw new MultilingualStringException(sprintf('Cannot set "%s" as default language because this is not one of declared available languages (%s)',
+                $language, implode(', ', self::$availableLanguages)));
         }
 
         return true;
     }
 
-    public function hydrate(array $translations) {
+    /**
+     * @param array $translations
+     */
+    public function hydrate(array $translations)
+    {
         $defaultValues = array_flip(self::$availableLanguages);
-        array_walk($defaultValues, function(&$value) { $value = '';});
+        array_walk($defaultValues, function (&$value) {
+            $value = '';
+        });
         $this->translations = $translations + $defaultValues;
     }
 
+    /**
+     * @return mixed
+     * @throws MultilingualStringException
+     */
+    public static function getDefaultLanguage()
+    {
+        if (empty(self::$defaultLanguage)) {
+
+            if (count(self::$availableLanguages)) {
+                return self::$availableLanguages[0];
+            } else {
+                throw new MultilingualStringException('No languages available');
+            }
+        }
+
+        return self::$defaultLanguage;
+    }
+
+    /**
+     * @return array
+     */
     public function extract()
     {
         return $this->translations;
+    }
+
+    /**
+     * @param null $lang
+     * @throws MultilingualStringException
+     */
+    public function getValue($lang = null)
+    {
+
+        if (is_null($lang)) {
+            $lang = self::getDefaultLanguage();
+        }
+
+        if (empty($this->translations[$lang])) {
+            $didTryFallbackLanguage = ($lang == self::getFallbackLanguage());
+            // try translation routes
+            while ($lang = self::getRoutedLanguage($lang)) {
+                if (!empty($this->translations[$lang])) {
+                    return $this->translations[$lang];
+                }
+                if (!$didTryFallbackLanguage) {
+                    $didTryFallbackLanguage = ($lang == self::getFallbackLanguage());
+                }
+            }
+
+            // look for fallback translation
+            if (!$didTryFallbackLanguage) {
+                if (!empty($this->translations[self::getFallbackLanguage()])) {
+                    return $this->translations[self::getFallbackLanguage()];
+                }
+            }
+
+            return null;
+        }
+
+        return $this->translations[$lang];
+
+
     }
 }
 
